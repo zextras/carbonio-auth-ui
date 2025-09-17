68f4138b2b6e8416a40f50e13a71f72fdf731b63
Dennis <dennis.ceron@zextras.com>
2025-09-17 11:38:16 +0200

    chore: general clenup

--- babel.config.app.js
+++ babel.config.app.js
@@ -1,6 +1,8 @@
-// SPDX-FileCopyrightText: 2022 Zextras <https://www.zextras.com>
-//
-// SPDX-License-Identifier: AGPL-3.0-only
+/*
+ * SPDX-FileCopyrightText: 2025 Zextras <https://www.zextras.com>
+ *
+ * SPDX-License-Identifier: AGPL-3.0-only
+ */
 
 module.exports = {
 	presets: ['@babel/preset-env', '@babel/preset-react', '@babel/preset-typescript'],
