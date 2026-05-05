60ead7339bd7a7d673b1d9a134e9a93b12fd9d39
Gabriele Marino <62339254+nubsthead@users.noreply.github.com>
2026-05-05 18:01:45 +0200

    build(deps): update dependencies (#170)

--- babel.config.app.js
+++ babel.config.app.js
@@ -9,8 +9,7 @@ module.exports = {
 	plugins: [
 		'@babel/plugin-transform-runtime',
 		'@babel/plugin-proposal-class-properties',
-		'babel-plugin-styled-components',
-		[
+[
 			'i18next-extract',
 			{
 				outputPath: 'translations/{{ns}}.json',
